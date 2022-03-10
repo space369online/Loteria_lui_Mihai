ee6ae3ea5556686ffd085f325f3db7573ac16d38
Ion Popescu <ion369369@gmail.com>
2022-03-10 22:55:16 +0200

    commit 1: UI Design

---
+++ app/src/main/java/com/ion369369/loterialuimihai/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ion369369.loterialuimihai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
